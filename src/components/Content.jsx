e7c32cc9cb1ff04bfebdcce3752f2ba29b02a147
Jimuelzxc <jimuelmarcgensaya@gmail.com>
2022-10-24 19:56:45 +0800

    asdasd

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import vid1 from '../assets/vid1.mp4'
 import vid2 from '../assets/vid2.mp4'
 const Content = () => {
     return (
-        <div className="parent bg-slate-400 h-full">
+        <div className="parent bg-slate-400 ">
             <Short className="bg-green-300" video={vid2} />
             <Short className="bg-green-300" video={vid1} />
         </div>
